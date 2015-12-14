d34d594982fa9097f63dc165cba9d4c0ff1888f9
m00nlight <dot_wangyushi@yeah.net>
2015-12-14 21:53:46 +0800

    code add installtion part for configuration

--- minizinc-mode.el
+++ minizinc-mode.el
@@ -15,6 +15,28 @@
 
 ;; Here are some example of configuration:
 
+;;; Installation:
+;;
+;; You can either manually install minizinc-mode or automatically
+;; install from melpa. Add the following line into your ~/.emacs
+;; file or any your emacs start file to install it from the melpa.
+;;
+;; (require 'package)
+;; (add-to-list 'package-archives
+;;              '("melpa" . "https://melpa.org/packages/"))
+;; (unless package-archive-contents (package-refresh-contents))
+;; (package-initialize)
+;;
+;; install minizinc-mode
+;;
+;; M-x package-install
+;; minizinc-mode
+;;
+;; Add the following lines to your emacs after installation.
+;; (require 'minizinc-mode)
+;; (add-to-list 'auto-mode-alist '("\\.mzn\\'" . minizinc-mode))
+
+
 ;;; License:
 
 ;; This program is free software; you can redistribute it and/or
